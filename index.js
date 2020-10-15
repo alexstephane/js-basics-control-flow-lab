2c24115e2aac24ffa55373350f2de173bffcd54b
alexstephane <alexngundji@gmail.com>
2020-10-15 00:23:15 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
+  if (`${ride}` < 400) {
+    return "This one is on me!"
+  } if (`${ride}`> 200) {
+    return 'I will gladly take your thirty bucks.'
+  } if (`${ride}`> 2500)  {
+    return 'No can do.'
+  } else {
+    
   // Write your code here!
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(NYC) {
+  if (NYC === NYC) {
+    return 'Ok, sounds good.'
+    
+  } if (NYC !== NYC) {
+    return  'No go.'
+  } else {
+    
+  } {
+    
+  }
   // Write your code here!
 }
 
